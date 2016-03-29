d351ef3f84c91ba9fdc51ea4d028fed771890641
andrewtatomyr <andrew.tatomyr@gmail.com>
2016-03-29 19:14:24 +0300

    10b

--- public/index.js
+++ public/index.js
@@ -384,7 +384,7 @@ function Companies() {
 			$(document.getElementById("toggleTree-"+id).firstChild).removeClass("glyphicon-plus");
 			$(document.getElementById("toggleTree-"+id).firstChild).addClass("glyphicon-minus");
 			for (var key in list) {
-				console.log(list[key].belongs2),"~",parseInt(id));//x
+				console.log(parseInt(list[key].belongs2),"~",parseInt(id));//x
 				if (parseInt(list[key].belongs2)===parseInt(id)) { //!
 					$("#"+key).removeClass("hidden");
 				}
